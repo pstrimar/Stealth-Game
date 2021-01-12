ea7d838c505856b7636f2755a3159ff75706bd56
pstrimar <pstrimar@gmail.com>
2021-01-12 17:40:34 -0500

    Initial commit

---
+++ Assets/Scripts/Guard.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guard : MonoBehaviour
+{
+    public static event Action OnGuardHasSpottedPlayer;
+
+    public float speed = 5f;
+    public float waitTime = .3f;
+    public float turnSpeed = 90f;
+    public float timeToSpotPlayer = .5f;
+    public Light spotlight;
+    public float viewDistance;
+    public LayerMask viewMask;
+
+    public Transform pathHolder;
+
+    float viewAngle;
+    float playerVisibleTimer;
+    Transform player;
+    Color originalSpotlightColor;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        viewAngle = spotlight.spotAngle;
+        originalSpotlightColor = spotlight.color;
+
+        Vector3[] waypoints = new Vector3[pathHolder.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = pathHolder.GetChild(i).position;
+            waypoints[i] = new Vector3(waypoints[i].x, transform.position.y, waypoints[i].z);
+        }
+
+        StartCoroutine(FollowPath(waypoints));
+    }
+
+    private void Update()
+    {
+        if (CanSeePlayer())
+        {
+            playerVisibleTimer += Time.deltaTime;
+        }
+        else
+        {
+            playerVisibleTimer -= Time.deltaTime;
+        }
+        playerVisibleTimer = Mathf.Clamp(playerVisibleTimer, 0, timeToSpotPlayer);
+        spotlight.color = Color.Lerp(originalSpotlightColor, Color.red, playerVisibleTimer / timeToSpotPlayer);
+
+        if (playerVisibleTimer >= timeToSpotPlayer)
+        {
+            OnGuardHasSpottedPlayer?.Invoke();
+        }
+    }
+
+    IEnumerator FollowPath(Vector3[] waypoints)
+    {
+        transform.position = waypoints[0];
+
+        int targetWaypointIndex = 1;
+        Vector3 targetWaypoint = waypoints[targetWaypointIndex];
+        transform.LookAt(targetWaypoint);
+
+        while (true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetWaypoint, speed * Time.deltaTime);
+            if (transform.position == targetWaypoint)
+            {
+                targetWaypointIndex = (targetWaypointIndex + 1) % waypoints.Length;
+                targetWaypoint = waypoints[targetWaypointIndex];
+                yield return new WaitForSeconds(waitTime);
+                yield return StartCoroutine(TurnToFace(targetWaypoint));
+            }
+            yield return null;
+        }
+    }
+
+    IEnumerator TurnToFace(Vector3 lookTarget)
+    {
+        Vector3 dirToLookTarget = (lookTarget - transform.position).normalized;
+
+        while (Vector3.Angle(transform.forward, dirToLookTarget) > Mathf.Epsilon)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(dirToLookTarget), turnSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private bool CanSeePlayer()
+    {
+        if (Vector3.Distance(transform.position, player.position) < viewDistance)
+        {
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            float angleToPlayer = Vector3.Angle(transform.forward, dirToPlayer);
+
+            if (angleToPlayer < viewAngle / 2f)
+            {
+                if (!Physics.Linecast(transform.position, player.position, viewMask))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 startPosition = pathHolder.GetChild(0).position;
+        Vector3 previousPosition = startPosition;
+
+        foreach (Transform waypoint in pathHolder)
+        {
+            Gizmos.DrawSphere(waypoint.position, .3f);
+            Gizmos.DrawLine(previousPosition, waypoint.position);
+            previousPosition = waypoint.position;
+        }
+        Gizmos.DrawLine(previousPosition, startPosition);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward * viewDistance);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public event Action OnReachedEndOfLevel;
+
+    public float moveSpeed = 7;
+    public float smoothTime = .1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothVelocity;
+    Vector3 velocity;
+
+    new Rigidbody rigidbody;
+    bool disabled;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        Guard.OnGuardHasSpottedPlayer += Disable;
+    }
+
+    void Update()
+    {
+        Vector3 inputDirection = Vector3.zero;
+        if (!disabled)
+        {
+            inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        }
+        
+        float inputMagnitude = inputDirection.magnitude;
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothVelocity, smoothTime);
+
+        float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+        angle = Mathf.LerpAngle(angle, targetAngle, Time.deltaTime * turnSpeed * inputMagnitude);
+
+        velocity = transform.forward * moveSpeed * smoothInputMagnitude;
+    }
+
+    void Disable()
+    {
+        disabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        Guard.OnGuardHasSpottedPlayer -= Disable;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            Disable();
+            OnReachedEndOfLevel?.Invoke();
+        }
+    }
+}
